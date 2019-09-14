f56a4197a4052a960659e59f9874b6edbad59431
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2019-09-14 10:31:37 +0700

    Initial commit

---
+++ lib/src/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ui_cinemax/res/color_app.dart';
+import 'package:flutter_ui_cinemax/src/ui/home/home_screen.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: ColorApp.primaryColor,
+        accentColor: ColorApp.accentColor,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
---
+++ lib/src/ui/home/home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Movies"),
+      ),
+      body: Center(
+        child: Text("Hello world"),
+      ),
+    );
+  }
+}
